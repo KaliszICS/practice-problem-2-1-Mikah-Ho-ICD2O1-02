398fdf1c55317b1441eacef33fdc7d3d98a44901
Mikah-Ho-ICD2O1-02 <mikah.ho@student.tdsb.on.ca>
2024-10-09 18:40:22 +0000

    hi mr kalisz :)

--- PP2_1.py
+++ PP2_1.py
@@ -1,13 +1,25 @@
-
+'''
+  Lesson: If Statements
+  Author: Mikah Ho
+  Date Created: October 9, 2024
+  Date Last Modified: October 9, 2024
+'''
 
 def q1(): 
   #Write Assignment code here
-
+  num = int(input("In: "))
+  if num % 2 == 0:
+    print(f"{num} is even")
+  if num % 2 != 0:
+    print(f"{num} is odd")
 
 def q2(): 
   #Write Assignment code here
-
-
+  name = input("In: ")
+  if name == "Kalisz":
+    print("teacher")
+  if name != "Kalisz":
+    print("student")
 
 #Do not alter the following code
 #Comment out the following code when running your tests
